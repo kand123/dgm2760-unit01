5a7c2e23c2942d1e979874d1ee8584fdff45213b
Kristin <kbrandtsojo@gmail.com>
2020-01-07 21:47:32 -0700

    added image and css styling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 document.querySelector('header > h1').innerText = "Joe's Bed and Breakfast"
-document.querySelector ('header > h2').innerText = "Best Slogan Ever"
+document.querySelector ('header > h2').innerText = "YOUR HOME AWAY FROM HOME"
 
 
 let userName = prompt("What is your name?")
 
-let message = `Hello ${userName}, welcome to the best lodging in Heber!`
+let message = `Hello ${userName}, welcome to the best lodging in Utah and your home away from home!`
 
 document.querySelector('#greeting').innerText = message
+
+document.querySelector('main > h1').innerText = "Welcome to Joe's"
+document.querySelector('main > h2').innerText = "What I Learned"
 
